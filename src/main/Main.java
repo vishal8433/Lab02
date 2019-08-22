a5c243790e02ae0dfebb61b67fe80b2cd4d789e7
vishal jain <44972151+vishal8433@users.noreply.github.com>
2019-08-22 15:15:25 +0530

    create object for scanner class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,15 @@
 package main;
 
+import java.util.Scanner;
+
 /**
  * @author vishal jain
  * @version 1.0
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(system.in);
         String userName = "";
-
+        scanner.close();
     }
 }
